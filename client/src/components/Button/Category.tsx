b6f6b3525141daf644920213aff5ea1b0a723860
YewonCALLI <96384200+YewonCALLI@users.noreply.github.com>
2025-12-13 20:02:15 +0900

    [update] 1213

--- client/src/components/Button/Category.tsx
+++ client/src/components/Button/Category.tsx
@@ -3,7 +3,7 @@ export default function Category() {
     <div className='fixed z-20 bottom-0 right-0'>
       <div className='bg-white'>
         <div className='flex items-center justify-around'>
-          <button className='bg-white w-[60px] lg:w-[100px] py-1 font-normal text-xl lg:text-2xl font-normal text-gray-400'>정보</button>
+          <button className='bg-white w-[60px] lg:w-[100px] py-1 text-xl lg:text-2xl font-normal text-gray-400'>정보</button>
           <button className='text-black w-[60px] lg:w-[100px] py-1 text-xl lg:text-2xl font-bold relative'>문서</button>
           <button className='text-gray-400 w-[60px] lg:w-[100px] py-1 text-xl lg:text-2xl font-normal relative'>일상</button>
         </div>
